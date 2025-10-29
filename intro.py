61eede339f537d9b14e5a07ac121508ac46eb9ec
kamalrajms <kamalrajindtn@gmail.com>
2025-10-29 12:57:11 +0530

    intro

---
+++ intro.py
@@ -0,0 +1,60 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Try programiz.pro")
+num1=float(input("enter a number 1 :"))
+num2=float(input("enter a number 2 :"))
+print(num1+num2)
+print(num1-num2)
+
+name="kumar"
+age=23
+city="chennai"
+
+x=10
+print(x)
+x=x+5
+print(x)
+
+a="hello"
+b=34
+c=55.5
+d=20j
+e=[1,2,3]
+f=(1,2,3)
+h=range(3)
+i={"name":"akaml"}
+j=True
+k=None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+
+firstname="hari"
+lastname="kriahnan"
+print(firstname+" "+lastname)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
