75ed1476d7b3a97c16b9b7d76a5ebbb914b398c5
kamalrajms <kamalrajindtn@gmail.com>
2025-10-30 13:30:00 +0530

    data types,operators type

---
+++ DataType.py
@@ -0,0 +1,109 @@
+# numerb type
+
+x=1
+y=2.8
+z=1j
+print(x)
+print(y)
+print(z)
+
+x=int(1)
+y=int(2.8)
+z=int("3")
+print(x)
+print(y)
+print(z)
+
+x=float(1)
+y=float(2.8)
+z=float("3")
+print(x)
+print(y)
+print(z)
+
+# string type
+
+# string
+
+a="hello"
+print(a)
+
+# slicing string
+
+b="hello world"
+print(b[2:5])
+print(b[:5])
+print(b[2:])
+print(b[-5:-2])
+
+# string Modifying
+
+a="Hello, World    "
+print(a.upper())
+print(a.lower())
+print(a.strip())
+
+#string concordination
+
+a="hello"
+b="world kumar"
+c=a+" "+b
+print(c)
+
+#string format
+
+c=34
+
+txt=f"my age is {c}"
+print(txt)
+
+## boolean type
+
+print(10>5)
+print(10==9)
+print(10>9)
+print(10>=9)
+print(10<9)
+print(10<=9)
+
+## none type
+
+a=None
+print(a)
+
+# operators
+# arithmetic opreator
+
+a=5
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+# assignement
+
+a=10
+b=5
+a+=b
+# a=a+b
+# a=a+3
+print(a)
+
+# Comparison operatior
+
+x=5
+y=6
+print(x==y)
+print(x>=y)
+print(x<y)
+
+#logical operatior
+
+x=55 
+y=56
+
+print((x>y)&(x<y))
